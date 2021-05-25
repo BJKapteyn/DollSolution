f0d9ce579e3c62a9bab614b1a22c57c0b7c60a1f
Brad <bjkapteyn@gmail.com>
2021-05-24 23:01:36 -0400

    Added FoundTargetList and began implementing PathFinder

--- DollSales/path_finder.go
+++ DollSales/path_finder.go
@@ -1,4 +1,4 @@
-package dolldelivery
+package main
 
 // PathFinder must be implemented
 type PathFinder func(startLocation, targetLocation string, streets []Street) (distance int, path []string)
@@ -8,6 +8,35 @@ type Street struct {
 	From     string
 	To       string
 	Distance int
+}
+
+func main() {
+	var FindPath PathFinder = func(startLocation, targetLocation string, streets []Street) (distance int, path []string) {
+		// Keep original array intact
+		neighborhood := streets
+		var nodesToCheck = PossiblePathList(startLocation, neighborhood)
+		var paths [][]Street
+		// Keep track of this to know when to add a new path array or when to add to existing one
+		var addedPaths = 0
+
+		// Create inital array of paths to be added to later
+		for _, s := range nodesToCheck {
+			path := []Street{s}
+			append(paths, path)
+		}
+
+	}
+
+}
+
+// For creating the inital array of booleans representing finished or deadend paths
+func FoundTargetList(numberOfStartingStreets int) []bool {
+	var boolArr [numberOfStartingStreets]bool
+
+	for i := 0; i < numberOfStartingStreets; i++ {
+		boolArr[i] := false
+	}
+	return boolArr
 }
 
 // Find target in array or streets stemming from node
@@ -29,9 +58,21 @@ func CalculateDistance(path []Street) {
 	}
 }
 
-// Create a list for next possible nodes.
-func NextNodelist(from string, streets []Street) []Street {
+// For removing a node from the neighborhood list after checking available neighboring nodes
+func RemoveNode(street string, streets []Street) {
 	var arr []Street
+
+	for _, v := range streets {
+		if v.To != street {
+			append(arr, v)
+		}
+	}
+}
+
+// Create a list for next possible nodes
+func PossiblePathList(from string, streets []Street) []Street {
+	var arr []Street
+
 	for _, v := range streets {
 		if v.From == from {
 			arr = append(arr, v)
